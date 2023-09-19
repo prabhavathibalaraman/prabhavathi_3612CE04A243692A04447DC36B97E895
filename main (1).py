132a7fd90699b13f3761db1fba047e3e90982b06
prabhavathibalaraman <144433022+prabhavathibalaraman@users.noreply.github.com>
2023-09-19 12:04:36 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,9 +1,32 @@
-def factorial(x):
-  """ This is a recursive function
-  to find the factorial of an integer"""
-  if x == 1:
-    return 1
-  else:
-    return(x * factorial(x-1)) 
-num = 3
-print("The factorial of ",num,"is", factorial(num)) 
+# Python program to create Bankaccount class
+# with both a deposit() and a withdraw() function
+class Bank_Account:
+    def __init__(self):
+        self.balance=0
+        print("Hello!!! Welcome to the Deposit & Withdrawal Machine")
+ 
+    def deposit(self):
+        amount=float(input("Enter amount to be Deposited: "))
+        self.balance += amount
+        print("\n Amount Deposited:",amount)
+ 
+    def withdraw(self):
+        amount = float(input("Enter amount to be Withdrawn: "))
+        if self.balance>=amount:
+            self.balance-=amount
+            print("\n You Withdrew:", amount)
+        else:
+            print("\n Insufficient balance  ")
+ 
+    def display(self):
+        print("\n Net Available Balance=",self.balance)
+ 
+# Driver code
+  
+# creating an object of class
+s = Bank_Account()
+  
+# Calling functions with that class object
+s.deposit()
+s.withdraw()
+s.display()
